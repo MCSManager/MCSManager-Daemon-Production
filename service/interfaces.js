dad88f3b3a37d12b196c39a2220dccfe0eaaa6bf
Suwings <suwings@outlook.com>
2022-01-29 17:05:02 +0800

    update code

--- service/interfaces.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export interface IForwardInstanceIO {
-//   sourceSocket: Socket,
-//   targetUuid: string
-// }
-//# sourceMappingURL=interfaces.js.map
